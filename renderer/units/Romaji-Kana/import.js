b3cc5dd771dcb6818a8461d95635540f94eb05b3
Tonton Pixel <tonton_pixel@Milou.local>
2019-11-11 00:41:27 +0100

    Updated app to version 1.4.4

--- renderer/units/Romaji-Kana/import.js
+++ renderer/units/Romaji-Kana/import.js
@@ -1,8 +1,11 @@
 //
 const unit = document.getElementById ('romaji-kana-unit');
 //
+const romajiHiraganaSelect = unit.querySelector ('.romaji-hiragana-select');
+const romajiKatakanaSelect = unit.querySelector ('.romaji-katakana-select');
 const romajiInput = unit.querySelector ('.romaji-input');
 const kanaOutput = unit.querySelector ('.kana-output');
+//
 const kanaInput = unit.querySelector ('.kana-input');
 const romajiOutput = unit.querySelector ('.romaji-output');
 //
@@ -15,20 +18,65 @@ module.exports.start = function (context)
 {
     const defaultPrefs =
     {
+        romajiHiraganaSelect: "kun-yomi",
+        romajiKatakanaSelect: "gairaigo",
         romajiInput: "",
         kanaInput: "",
-        examples: true,
+        examples: false,
         references: false
     };
     let prefs = context.getPrefs (defaultPrefs);
     //
     const { romajiToKana, kanaToRomaji } = require ('simple-romaji-kana');
     //
-    kanaOutput.value = romajiToKana (romajiInput.value = prefs.romajiInput);
-    romajiInput.addEventListener ('input', (event) => { kanaOutput.value = romajiToKana (event.currentTarget.value); });
+    // Rōmaji to Kana
     //
-    romajiOutput.value = kanaToRomaji (kanaInput.value = prefs.kanaInput);
-    kanaInput.addEventListener ('input', (event) => { romajiOutput.value = kanaToRomaji (event.currentTarget.value); });
+    function updateKanaOutput (romajiValue)
+    {
+        let options = { hiragana: romajiHiraganaSelect.value, katakana: romajiKatakanaSelect.value };
+        kanaOutput.value = romajiToKana (romajiValue, options);
+    }
+    //
+    romajiHiraganaSelect.value = prefs.romajiHiraganaSelect;
+    if (romajiHiraganaSelect.selectedIndex < 0) // -1: no element is selected
+    {
+        romajiHiraganaSelect.selectedIndex = 0;
+    }
+    romajiHiraganaSelect.addEventListener ('input', (event) => { updateKanaOutput (romajiInput.value); });
+    //
+    romajiKatakanaSelect.value = prefs.romajiKatakanaSelect;
+    if (romajiKatakanaSelect.selectedIndex < 0) // -1: no element is selected
+    {
+        romajiKatakanaSelect.selectedIndex = 0;
+    }
+    romajiKatakanaSelect.addEventListener ('input', (event) => { updateKanaOutput (romajiInput.value); });
+    //
+    updateKanaOutput (romajiInput.value = prefs.romajiInput);
+    romajiInput.addEventListener
+    (
+        'input',
+        (event) =>
+        {
+            updateKanaOutput (event.currentTarget.value);
+        }
+    );
+    //
+    // Kana to Rōmaji
+    //
+    function updateRomajiOutput (kanaValue)
+    {
+        romajiOutput.value = kanaToRomaji (kanaValue);
+    }
+    //
+    updateRomajiOutput (kanaInput.value = prefs.kanaInput);
+    kanaInput.addEventListener
+    (
+        'input',
+        (event) =>
+        {
+            updateRomajiOutput (event.currentTarget.value);
+        }
+    );
     //
     examples.open = prefs.examples;
     //
@@ -44,6 +92,8 @@ module.exports.stop = function (context)
 {
     let prefs =
     {
+        romajiHiraganaSelect: romajiHiraganaSelect.value,
+        romajiKatakanaSelect: romajiKatakanaSelect.value,
         romajiInput: romajiInput.value,
         kanaInput: kanaInput.value,
         examples: examples.open,
